ce90a1278be4e66b9391f318d0858c62b64f9b49
2lavea02 <2lavea02@solent.ac.uk>
2018-04-24 15:49:08 +0100

    2

--- app/src/main/java/com/example/a2lavea02/solenteatout/addrestaurant.java
+++ app/src/main/java/com/example/a2lavea02/solenteatout/addrestaurant.java
@@ -3,6 +3,7 @@ package com.example.a2lavea02.solenteatout;
 /**
  * Created by 2lavea02 on 23/04/2018.
  */
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -24,12 +25,25 @@ public class addrestaurant extends AppCompatActivity implements View.OnClickList
     }
     public void onClick(View view){
 
+
+        Intent intent = new Intent();
+        Bundle bundle = new Bundle();
+
         EditText restname = (EditText)findViewById(R.id.sl1);
         EditText restaddress = (EditText)findViewById(R.id.sl2);
         EditText restcuisine = (EditText)findViewById(R.id.sl3);
         EditText restrating = (EditText)findViewById(R.id.sl4);
 
+        String stringrestname = restname.getText().toString();
+        String stringrestadd = restaddress.getText().toString();
+        String stringcuisine = restcuisine.getText().toString();
+        String stringrating = restrating.getText().toString();
 
+        bundle.putString("com.example.a2lavea02.solenteatout.RestName", stringrestname);
+        bundle.putString("com.example.a2lavea02.solenteatout.RestAdd", stringrestadd);
+        bundle.putString("com.example.a2lavea02.solenteatout.RestCuisine", stringcuisine);
+        bundle.putString("com.example.a2lavea02.solenteatout.RestRating", stringrating);
+        // create an onactivityresult within the main activity.
     }
 
 
