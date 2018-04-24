425b51c725eee059fda25baf76c4392654a97dab
2lavea02 <2lavea02@solent.ac.uk>
2018-04-24 13:52:34 +0100

    2

--- app/src/main/java/com/example/a2lavea02/solenteatout/addrestaurant.java
+++ app/src/main/java/com/example/a2lavea02/solenteatout/addrestaurant.java
@@ -7,18 +7,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 import android.view.MenuInflater;
 
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
-public class addrestaurant extends AppCompatActivity {
+public class addrestaurant extends AppCompatActivity implements View.OnClickListener {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_restaurant);
 
     }
+    public void onClick(View view){
+
+        EditText restname = (EditText)findViewById(R.id.sl1);
+        EditText restaddress = (EditText)findViewById(R.id.sl2);
+        EditText restcuisine = (EditText)findViewById(R.id.sl3);
+        EditText restrating = (EditText)findViewById(R.id.sl4);
+
+
+    }
+
 
 }
